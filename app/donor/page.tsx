abb1b6f281a1ace4a5e0ac17568f50db23e526ec
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-02-10 14:46:28 +0530

    Manage Color Scheme

--- app/donor/page.tsx
+++ app/donor/page.tsx
@@ -4,7 +4,7 @@ const HomePage = () => {
   return (
     <div className="p-5">
       <h1 className="font-bold text-6xl text-center py-40 px-10">
-        Welcome User to <span className="text-primary-400">Regift</span>.
+        Welcome User to <span className="text-secondary-500">Regift</span>.
       </h1>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 import { nextui } from "@nextui-org/react";
 
 const config: Config = {
@@ -12,6 +13,37 @@ const config: Config = {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "light", // default theme from the themes object
+      defaultExtendTheme: "light",
+      themes: {
+        light: {
+          colors: {
+            background: colors.slate[50],
+            foreground: colors.black,
+            primary: { ...colors.pink, DEFAULT: colors.pink["600"], foreground: "#FFFFFF" },
+            secondary: colors.indigo,
+            warning: colors.amber,
+            success: colors.emerald,
+            danger: colors.red,
+          },
+        },
+        dark: {
+          colors: {
+            background: "#111111",
+            foreground: colors.white,
+            primary: { ...colors.pink, DEFAULT: colors.pink["600"], foreground: "#FFFFFF" },
+            secondary: colors.indigo,
+            warning: colors.amber,
+            success: colors.emerald,
+            danger: colors.red,
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
