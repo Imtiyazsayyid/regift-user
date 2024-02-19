c9aaeed29919f5371a23128f5fcd99945d33ca77
Imtiyaz Sayyid <imtiyazsayyidwork@gmail.com>
2024-02-19 17:58:52 +0530

    Make PWA

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+
 const nextConfig = {
+  ...withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }),
   env: {
     // BASE_URL: "http://localhost:8003/api/admin",
     // BASE_URL: "https://regift-backend.vercel.app/api/admin",
